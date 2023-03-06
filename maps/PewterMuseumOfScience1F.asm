8e890f4f66905cec83a83d2bd124a8aff8403e00
Nerd Inside <55431253+N3rd1n5id3@users.noreply.github.com>
2023-03-06 17:21:15 +0100

    Update PewterMuseumOfScience1F.asm

--- maps/PewterMuseumOfScience1F.asm
+++ maps/PewterMuseumOfScience1F.asm
@@ -11,23 +11,3 @@ PewterMuseumOfScience1F_MapScriptHeader:
 	warp_event  7,  7, PEWTER_MUSEUM_OF_SCIENCE_2F, 1
 
 	def_coord_events
-
-	def_bg_events
-	bg_event  2,  3, BGEVENT_READ, KabutopsFossilSignpostScript
-	bg_event  3,  6, BGEVENT_READ, AerodactylFossilSignpostScript
-	bg_event 12,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-	bg_event 13,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-	bg_event 14,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-	bg_event 15,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-	bg_event 17,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-	bg_event 18,  1, BGEVENT_JUMPTEXT, Museum1FBookshelfSignpostText
-
-	def_object_events
-	object_event 18,  3, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, Museum1FFossilScientistScript, -1
-	object_event 12,  4, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Museum1FReceptionistScript, -1
-	object_event 16,  2, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_COMMAND, jumptextfaceplayer, Museum1FScientistText, -1
-	object_event  1,  7, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, Museum1FGrampsText, -1
-	object_event  4,  3, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, Museum1FYoungsterText, -1
-
-	object_const_def
-	const PEWTERMUSEUMOFSCIENCE1F_SCIENTIST2
