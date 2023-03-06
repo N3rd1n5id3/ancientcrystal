92fa135dd4ab5ee1a1001261062811ca28b1d6e6
Nerd Inside <55431253+N3rd1n5id3@users.noreply.github.com>
2023-03-06 19:41:45 +0100

    Update PewterMuseumOfScience2F.asm

--- maps/PewterMuseumOfScience2F.asm
+++ maps/PewterMuseumOfScience2F.asm
@@ -1,9 +1,18 @@
-PewterMuseumOfScience2F_MapScriptHeader:
+	object_const_def
+
+PewterMuseumOfScience2F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+PewterMuseumOfScience12_MapEvents:
+	db 0, 0 ; filler
 
 	def_warp_events
 	warp_event  7,  7, PEWTER_MUSEUM_OF_SCIENCE_1F, 5
 
 	def_coord_events
+
+	def_bg_events
+
+	def_object_events
