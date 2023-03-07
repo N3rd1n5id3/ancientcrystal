9ca72069e3d1bb9ea4d3e225d249713f0a0fd093
Nerd Inside <55431253+N3rd1n5id3@users.noreply.github.com>
2023-03-07 15:46:08 +0100

    Update PewterMuseumOfScience2F.asm

--- maps/PewterMuseumOfScience2F.asm
+++ maps/PewterMuseumOfScience2F.asm
@@ -14,5 +14,81 @@ PewterMuseumOfScience2F_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event  3,  6, BGEVENT_READ, Museum2FMoonStoneSignpostScript
+	bg_event  6,  3, BGEVENT_READ, OmastarFossilSignpostScript
 
 	def_object_events
+	object_event  7,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, jumptextfaceplayer, Museum2FScientistScript, -1
+	object_event  1,  7, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, jumptextfaceplayer, Museum2FYoungsterScript, -1
+	object_event  2,  1, SPRITE_BEAUTY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, jumptextfaceplayer, Museum2FBeautyScript, -1
+	object_event 12,  5, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Museum2FCooltrainerMScript, -1
+	
+	Museum2FScientistScript
+	faceplayer
+	opentext
+	writetext Museum2FScientistText
+	
+	Museum2FYoungsterScript
+	faceplayer
+	opentext
+	writetext Museum2FChildText
+	
+	Museum2FBeautyScript
+	faceplayer
+	opentext
+	writetext Museum2FBeautyText
+	
+	Museum2FCooltrainerMScript
+	faceplayer
+	opentext
+	writetext Museum2FCooltrainerMText
+	
+	Museum2FScientistText:
+	text "Meteorites struck"
+	line "Mt.Moon near"
+	cont "Pewter City."
+
+	para "We have one"
+	line "on display."
+
+	para "It seems to emit"
+	line "strange energy…"
+	done
+	
+	Museum2FChildText:
+	text "Stuff and rocks"
+	line "and rocks and"
+
+	para "stuff. Can we go"
+	line "already?"
+	done
+	
+	Museum2FBeautyText:
+	text "#mon revived"
+	line "from fossils are"
+
+	para "all part Rock-"
+	line "type."
+
+	para "Were they always"
+	line "that way, or is it"
+
+	para "a result of the"
+	line "fossilization"
+	cont "process?"
+	done
+	
+	Museum2FCooltrainerMText:
+	text "This space shuttle"
+	line "used to fly 250"
+
+	para "kilometers above"
+	line "the earth, defy-"
+	cont "ing gravity!"
+
+	para "You know what else"
+	line "can defy gravity?"
+
+	para "Seismic Toss,"
+	line "of course!"
+	done
