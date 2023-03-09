09c0900d40897531977a652911eae6ec4ba50b1b
Nerd Inside <55431253+N3rd1n5id3@users.noreply.github.com>
2023-03-09 09:30:19 +0100

    Update PokecomCenterAdminOfficeMobile.asm

--- maps/PokecomCenterAdminOfficeMobile.asm
+++ maps/PokecomCenterAdminOfficeMobile.asm
@@ -2,11 +2,16 @@
 	const POKECOMCENTERADMINOFFICEMOBILE_SCIENTIST1
 	const POKECOMCENTERADMINOFFICEMOBILE_SCIENTIST2
 	const POKECOMCENTERADMINOFFICEMOBILE_SCIENTIST3
+	const POKECOMCENTERADMINOFFICEMOBILE_NURSE
 
 PokecomCenterAdminOfficeMobile_MapScripts:
 	def_scene_scripts
+	scene_const SCENE_POKECOMCENTERADMINOFFICEMOBILE_GS_BALL
 
 	def_callbacks
+
+PokecomCenterAdminOfficeMobileNurseScript:
+
 
 PokecomCenterAdminOfficeMobileScientist1Script:
 	jumptextfaceplayer PokecomCenterAdminOfficeMobileScientist1Text
@@ -16,6 +21,294 @@ PokecomCenterAdminOfficeMobileScientist2Script:
 
 PokecomCenterAdminOfficeMobileScientist3Script:
 	jumptextfaceplayer PokecomCenterAdminOfficeMobileScientist3Text
+	
+PokecomCenterAdminOfficeMobile_GSBallSceneLeft:
+	checkevent EVENT_BEAT_ELITE_FOUR
+	iftrue .gsball
+	end
+
+.gsball
+	checkevent EVENT_GOT_GS_BALL_FROM_POKECOM_CENTER
+	iftrue .cancel
+	playsound SFX_EXIT_BUILDING
+	moveobject POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, 0, 7
+	disappear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	appear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	playmusic MUSIC_SHOW_ME_AROUND
+	applymovement POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, PokecomCenterAdminOfficeMobileLinkReceptionistApproachPlayerAtLeftDoorwayTileMovement
+	turnobject PLAYER, UP
+	opentext
+	writetext PokecomCenterAdminOfficeMobileLinkReceptionistPleaseAcceptGSBallText
+	waitbutton
+	verbosegiveitem GS_BALL
+	setevent EVENT_GOT_GS_BALL_FROM_POKECOM_CENTER
+	setevent EVENT_CAN_GIVE_GS_BALL_TO_KURT
+	writetext PokecomCenterAdminOfficeMobileLinkReceptionistPleaseDoComeAgainText
+	waitbutton
+	closetext
+	applymovement POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, PokecomCenterAdminOfficeMobileLinkReceptionistWalkToStairsFromLeftDoorwayTileMovement
+	special RestartMapMusic
+	disappear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	playsound SFX_EXIT_BUILDING
+.cancel
+	end
+
+PokecomCenterAdminOfficeMobile_GSBallSceneRight:
+	checkevent EVENT_BEAT_ELITE_FOUR
+	iftrue .gsball
+	end
+
+.gsball
+	checkevent EVENT_GOT_GS_BALL_FROM_POKECOM_CENTER
+	iftrue .cancel
+	playsound SFX_EXIT_BUILDING
+	moveobject POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, 0, 7
+	disappear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	appear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	playmusic MUSIC_SHOW_ME_AROUND
+	applymovement POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, PokecomCenterAdminOfficeMobileLinkReceptionistApproachPlayerAtLeftDoorwayTileMovement
+	turnobject PLAYER, UP
+	opentext
+	writetext PokecomCenterAdminOfficeMobileLinkReceptionistPleaseAcceptGSBallText
+	waitbutton
+	verbosegiveitem GS_BALL
+	setevent EVENT_GOT_GS_BALL_FROM_POKECOM_CENTER
+	setevent EVENT_CAN_GIVE_GS_BALL_TO_KURT
+	writetext PokecomCenterAdminOfficeMobileLinkReceptionistPleaseDoComeAgainText
+	waitbutton
+	closetext
+	applymovement POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST, PokecomCenterAdminOfficeMobileLinkReceptionistWalkToStairsFromLeftDoorwayTileMovement
+	special RestartMapMusic
+	disappear POKECOMCENTERADMINOFFICEMOBILE_LINK_RECEPTIONIST
+	playsound SFX_EXIT_BUILDING
+.cancel
+	end
+	
+	
+	para "But they're still"
+	line "adjusting things."
+	done
+
+GoldenrodPokecomCenterPerson4Text: ; unreferenced
+	text "Some girl I don't"
+	line "know sent me her"
+
+	para "HOPPIP."
+	line "You should trade"
+
+	para "for a #MON that"
+	line "you want."
+	done
+
+GoldenrodPokecomCenterPerson5Text: ; unreferenced
+	text "I received a"
+	line "female HOPPIP, but"
+	cont "its named STANLEY!"
+
+	para "That's my dad's"
+	line "name!"
+	done
+
+GoldenrodPokecomCenterPerson6Text: ; unreferenced
+	text "What is the NEWS"
+	line "MACHINE?"
+
+	para "Does it get news"
+	line "from a wider area"
+	cont "than the radio?"
+	done
+
+GoldenrodPokecomCenterPerson7Text: ; unreferenced
+	text "The #COM CENTER"
+	line "will link with all"
+
+	para "#MON CENTERS in"
+	line "a wireless net."
+
+	para "That must mean"
+	line "I'll be able to"
+
+	para "link with all"
+	line "sorts of people."
+	done
+
+GoldenrodPokecomCenterPerson8Text: ; unreferenced
+	text "The machines here"
+	line "can't be used yet."
+
+	para "Still, it's nice"
+	line "coming to a trendy"
+
+	para "place before other"
+	line "people."
+	done
+
+GoldenrodPokecomCenterPerson9Text: ; unreferenced
+	text "My friend was in"
+	line "the NEWS a while"
+
+	para "back. I was really"
+	line "surprised!"
+	done
+
+GoldenrodPokecomCenterPerson10Text: ; unreferenced
+	text "I get anxious if I"
+	line "don't check out"
+	cont "the latest NEWS!"
+	done
+
+GoldenrodPokecomCenterPerson11Text: ; unreferenced
+	text "If I get in the"
+	line "NEWS and become"
+
+	para "famous, I bet I'll"
+	line "be adored."
+
+	para "I wonder how I"
+	line "could get in the"
+	cont "NEWS?"
+	done
+
+GoldenrodPokecenter1FGameboyKidText:
+	text "The COLOSSEUM"
+	line "upstairs is for"
+	cont "link battles."
+
+	para "Battle records are"
+	line "posted on the"
+
+	para "wall, so I can't"
+	line "afford to lose."
+	done
+
+GoldenrodPokecomCenterPerson12Text: ; unreferenced
+	text "I came over here"
+	line "when I got word"
+
+	para "that GOLDENROD's"
+	line "#MON CENTER has"
+
+	para "new machines that"
+	line "no one's ever seen"
+	cont "before."
+
+	para "But it looks like"
+	line "they're still busy"
+
+	para "with all their"
+	line "preparations…"
+	done
+
+GoldenrodPokecomCenterPerson13Text: ; unreferenced
+	text "Just seeing all"
+	line "these new things"
+
+	para "here makes me feel"
+	line "younger!"
+	done
+
+GoldenrodPokecenter1FLassText:
+	text "A higher level"
+	line "#MON doesn't"
+	cont "always win."
+
+	para "After all, it may"
+	line "have a type dis-"
+	cont "advantage."
+
+	para "I don't think"
+	line "there is a single"
+
+	para "#MON that is"
+	line "the toughest."
+	done
+
+GoldenrodPokeCenter1FLinkReceptionistPleaseAcceptGSBallText:
+	text "<PLAYER>, isn't it?"
+
+	para "Congratulations!"
+
+	para "As a special deal,"
+	line "a GS BALL has been"
+	cont "sent just for you!"
+
+	para "Please accept it!"
+	done
+
+GoldenrodPokeCenter1FLinkReceptionistPleaseDoComeAgainText:
+	text "Please do come"
+	line "again!"
+	done
+
+GoldenrodPokecomCenterSignText: ; unreferenced
+	text "#COM CENTER"
+	line "1F INFORMATION"
+
+	para "Left:"
+	line "ADMINISTRATION"
+
+	para "Center:"
+	line "TRADE CORNER"
+
+	para "Right:"
+	line "#MON NEWS"
+	done
+
+GoldenrodPokecomCenterNewsMachineNotYetText: ; unreferenced
+	text "It's a #MON"
+	line "NEWS MACHINE!"
+
+	para "It's not in"
+	line "operation yet…"
+	done
+
+GoldenrodPokecenter1FPokefanFDoYouHaveEonMailText:
+	text "Oh my, your pack"
+	line "looks so heavy!"
+
+	para "Oh! Do you happen"
+	line "to have something"
+	cont "named EON MAIL?"
+
+	para "My daughter is"
+	line "after one."
+
+	para "You can part with"
+	line "one, can't you?"
+	done
+
+GoldenrodPokecenter1FAskGiveAwayAnEonMailText:
+	text "Give away an EON"
+	line "MAIL?"
+	done
+
+GoldenrodPokecenter1FPokefanFThisIsForYouText:
+	text "Oh, that's great!"
+	line "Thank you, honey!"
+
+	para "Here, this is for"
+	line "you in return!"
+	done
+
+GoldenrodPokecenter1FPokefanFDaughterWillBeDelightedText:
+	text "My daughter will"
+	line "be delighted!"
+	done
+
+GoldenrodPokecenter1FPokefanFTooBadText:
+	text "Oh? You don't have"
+	line "one? Too bad."
+	done
+
+GoldenrodPokecenter1FPokefanFAnotherTimeThenText:
+	text "Oh… Well, another"
+	line "time, then."
+	done
+
+GoldenrodPokecenter1FPlayerGaveAwayTheEonMailText:
+	text "<PLAYER> gave away"
+	line "the EON MAIL."
+	done
 
 PokecomCenterAdminOfficeMobileComputer1:
 	opentext
